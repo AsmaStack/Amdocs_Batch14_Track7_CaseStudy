bd976700c09310804e9bd9e35b30c3df4a744ce7
sanchalika <sanchalika2019@gmail.com>
2023-08-25 14:04:43 +0530

    Final Case Study

---
+++ Sanchalika/src/main/java/com/amdocs/SpringBootCustomer/model/Orders.java
@@ -0,0 +1,72 @@
+package com.amdocs.SpringBootCustomer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.sql.Date;
+
+@Entity
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private Date time_of_order;
+    private double order_cost;
+    private String order_status;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customers customer;
+
+	public Orders() {
+		
+	}
+	
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public Date getTime_of_order() {
+		return time_of_order;
+	}
+
+	public void setTime_of_order(Date time_of_order) {
+		this.time_of_order = time_of_order;
+	}
+
+	public double getOrder_cost() {
+		return order_cost;
+	}
+
+	public void setOrder_cost(double order_cost) {
+		this.order_cost = order_cost;
+	}
+
+	public String getOrder_status() {
+		return order_status;
+	}
+
+	public void setOrder_status(String order_status) {
+		this.order_status = order_status;
+	}
+
+	public Customers getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customers customer) {
+		this.customer = customer;
+	}
+	
+	
+
+  
+}
